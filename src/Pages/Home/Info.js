f2e3600f9af4bcccc3d7ef20174cd19505903bba
ImranHossain5722 <imd572258@gmail.com>
2022-05-10 21:30:55 +0600

    fixed error infoCard

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -7,9 +7,9 @@ import phone from "../../assets/icons/phone.svg";
 const Info = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 ">
-      <InfoCard cardTitle="Opening Hours" bgclassName="bg-gradient-to-r from-secondary to-primary"  img={clock}></InfoCard>
-      <InfoCard cardTitle="Visit our location"bgclassName="bg-accent"  img={marker}></InfoCard>
-      <InfoCard cardTitle="Contact us now" bgclassName="bg-secondary"  img={phone}></InfoCard>
+      <InfoCard cardTitle="Opening Hours" bgClass ="bg-gradient-to-r from-secondary to-primary"  img={clock}></InfoCard>
+      <InfoCard cardTitle="Visit our location"bgClass="bg-accent"  img={marker}></InfoCard>
+      <InfoCard cardTitle="Contact us now"bgClass ="bg-secondary"  img={phone}></InfoCard>
     </div>
   );
 };
